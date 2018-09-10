1b8b4e10bd02b9eec2f76a59a8d93fe3e340f1d8
hyuan13 <hyuan13@illinois.edu>
2018-09-09 21:10:04 -0500

    client

---
+++ client.go
@@ -0,0 +1,95 @@
+package main
+
+import "net"
+import "fmt"
+//import "bufio"
+import "os"
+import "io"
+import "encoding/json" 
+import "io/ioutil"
+import "strconv"
+import "sync"
+
+
+func main(){
+	//get grep command and port number from command-line arguments
+	if len(os.Args) < 3 {
+		fmt.Println("Please type in grep command and port number!")
+		return
+	}
+	grep_cmd := os.Args[1]
+	port_num := os.Args[2]
+
+	//parse json file get each server information
+	jsonFile, err := os.Open("test.json") 
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile) 
+	var server_info Servsers
+	json.Unmarshal(byteValue, &server_info)
+	for i := 0; i < len(server_info.Servsers); i++ {
+		fmt.Println("Server Id: " + strconv.Itoa(server_info.Servsers[i].Id))
+		fmt.Println("Server Hostname: " + server_info.Servsers[i].Hostname)
+		fmt.Println("Server Logfile: " + server_info.Servsers[i].Logfile)
+	}
+	
+	//build a connect with each server
+	var wg sync.WaitGroup
+	wg.Add(len(server_info.Servsers))
+	for i := 0; i < len(server_info.Servsers); i++ {
+		//go work(server_info.Servsers[i].Hostname, server_info.Servsers[i].Logfile, grep_cmd, port_num)
+		go func(Hostname string, Logfile string, grep_cmd string, port_num string, i int){
+			//connect to server
+			conn, _ := net.Dial("tcp", Hostname + ":" + port_num)
+			//send to socket
+			name := "machine" + strconv.Itoa(i + 1) + ".i.log"
+			fmt.Fprintf(conn, grep_cmd + " " + name)
+			//fmt.Fprintf(conn, grep_cmd)
+			//create log file
+			f, err := os.Create(Logfile)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer f.Close()
+	
+			//read message from socket and write to log file
+			for true {
+				message := make([]byte, 1024)
+				n1, err := conn.Read(message)
+				if err != nil {
+					if err == io.EOF {
+						break	
+					}
+					fmt.Println(err)
+					os.Exit(1)	
+				}
+				text := string(message[:n1])
+				fmt.Println(text)
+				n2, err := f.WriteString(text)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				_ = n1
+				_ = n2
+			}	
+			wg.Done()	
+		}(server_info.Servsers[i].Hostname, server_info.Servsers[i].Logfile, grep_cmd, port_num, i)		
+	}
+	wg.Wait()
+	fmt.Println("done")
+}
+
+type Servsers struct {
+	Servsers []serverInfo `json:"server_list"`
+}
+
+type serverInfo struct {
+	Id    int    `json:"id"`
+	Hostname   string `json:"hostname"`
+	Logfile   string `json:"logfile"`
+}
