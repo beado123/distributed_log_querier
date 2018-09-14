2b4e664abd10c8b31fb8c5cac585cd29e8365038
Yuan <hyuan13@fa18-cs425-g69-01.cs.illinois.edu>
2018-09-14 16:58:22 -0500

    client

--- client_test.go
+++ client_test.go
@@ -8,13 +8,19 @@ import "strconv"
 import "strings"
 
 func TestDifference(t *testing.T){
-	queries := [1]string{"5qd"}
+	queries := [9]string{"apple", "apple", "apple", "apple", "apple", "apple", "apple", "apple", "apple"}
+	filenames := [9]string{"rareOne.log","rareSome.log","rareAll.log","somewhatFrequentOne.log","somewhatFrequentSome.log","somewhatFrequentAll.log","frequentOne.log","frequentSome.log","frequentAll.log"}
+	outputs := [9]int{1, 3, 10, 4, 12, 40, 5, 15, 50}
+	numbers := [10]string{"01", "02", "03", "04", "05", "06", "07", "08", "09", "10"}
+	//queries := [1]string{"apple"}
+	//filenames := [1]string{"rareSome.log"}
+	//outputs := [1]int{3}
 	
 	for i := 0; i < len(queries); i++ {
-		exec.Command("./client", queries[i], "3000").Run();
+		exec.Command("./client", queries[i], "3000", filenames[i]).Run();
 		ret := 0
-		for j := 1; j <= 10; j++ {
-			filename := "machine" + strconv.Itoa(j)+ ".i.log"
+		for j := 0; j < 10; j++ {
+			filename := "machine" + numbers[j] + ".i.log"
 			if _, err := os.Stat(filename); err == nil {	
 				cmd := exec.Command("wc", "-l", filename)
 				count, err := cmd.CombinedOutput()
@@ -26,6 +32,24 @@ func TestDifference(t *testing.T){
 			}
 		}
 		fmt.Println(ret)
+		if ret != outputs[i] {
+			t.Errorf("Test failed", filenames[i])
+		}
+		for j := 0; j < 10; j++ {
+                        filename := "machine" + numbers[j] + ".i.log"
+			if _, err := os.Stat(filename); err == nil {
+				deleteFile(filename)
+			}
+		}
 	}
+}
+
+func deleteFile(f string) {
+        if _, err := os.Stat("./" + f); err == nil {
+                err := os.Remove("./" + f)
+                //check(err)
+                fmt.Println("Deleted " + f)
+		_ = err
+        }
 }
 
